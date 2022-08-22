a6c1b79d12802e7d56fbf55fcb2cb031e404893c
engelhard <engelhard25@hotmail.com>
2022-08-22 19:29:11 -0400

    Segundo Proyecto openbootcamp

---
+++ index.js
@@ -0,0 +1,22 @@
+//Segundo proyecto openbootcamp
+
+const Nombre = "Engelhard Pacheco";
+const Edad = 25;
+const Desarrollador = true;
+const fecha_nacimiento = new Date ("july 25 1997");
+const libro_favorito = {
+
+    titulo: "Quien se ha llevado mi queso",
+    autor: "Spencer Jhonson",
+    fecha: new Date("sep 8 1999"),
+    url: "https://es.wikipedia.org/wiki/%C2%BFQui%C3%A9n_se_ha_llevado_mi_queso%3F",
+}
+
+console.log(Nombre)
+console.log(Edad)
+console.log(Desarrollador)
+console.log(fecha_nacimiento)
+console.log(libro_favorito.titulo)
+console.log(libro_favorito.autor)
+console.log(libro_favorito.fecha)
+console.log(libro_favorito.url)
